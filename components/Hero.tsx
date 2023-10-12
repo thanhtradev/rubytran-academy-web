b06a8ee9c58d61359213992c327f0bbc7c941854
thanhtradev <thanhtra2001.stt@gmail.com>
2023-10-13 00:03:56 +0700

    hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,17 @@ import Image from 'next/image';
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='hero'>
+      <img className="w-[598px] h-[411px]" src="https://via.placeholder.com/598x411" />
+      <div className='flex-1 padding-x items-center justify-center h-[139px] text-center flex-col pt-20'>
+        <p className='hero__top-title'>HỌC VIỆN THẨM MỸ RUBY TRAN</p>
+        <h1 className='hero__title'>
+          RUBY TRAN ACADEMY
+        </h1>
+        <p className='hero__subtitle'>CHẤP CÁNH TƯƠNG LAI VƯƠN XA</p>
+      </div>
+      <img className="w-[352px] h-[527px]" src="https://via.placeholder.com/352x527" />
+    </div>
   )
 }
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,7 @@ const config: Config = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  mode: 'jit',
   theme: {
     extend: {
       fontFamily:{
@@ -16,6 +17,11 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors:{
+        "primary-yellow": "#FFDE81",
+        "secondary-green": "#1C6463",
+        "tertiary-orange-light": "#F8F0E6",
+      }
     },
   },
   plugins: [],
